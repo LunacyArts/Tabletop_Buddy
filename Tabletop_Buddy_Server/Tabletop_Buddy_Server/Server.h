ecc90290cc34aaf871dd68ee5f3fd61d5124b2d9
Roger Baumbach II <rogera24@hotmail.com>
2017-05-12 14:20:29 -0400

    Removed git conflict tags

--- Tabletop_Buddy_Server/Tabletop_Buddy_Server/Server.h
+++ Tabletop_Buddy_Server/Tabletop_Buddy_Server/Server.h
@@ -8,12 +8,9 @@
 #include <vector>
 #include <iostream>
 #include <string>
-<<<<<<< HEAD
 #include <time.h>
-=======
 #include <chrono>
 #include <thread>
->>>>>>> origin/master
 using namespace std;
 
 class Server {
@@ -28,15 +25,12 @@ class Server {
 			ServerComms sc(L"http://192.168.254.200:9000/");
 			sc.start_server();
 			std::cout << "Server started.\n";
-<<<<<<< HEAD
 			//Set up random server seed
 			srand(time(NULL));
-=======
 			while (1) {
 				std::this_thread::sleep_for(std::chrono::milliseconds(3000));
 				std::cout << "Main thread still going" << endl;
 			}
->>>>>>> origin/master
 		}
 
 		//Lobby creation, requires a name and gm_id to create a lobby, generates a 6 digit pin using get_key(), then pushes it into vector turn_order
